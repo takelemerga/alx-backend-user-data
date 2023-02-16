0395f93de87872c544fc0be7f9b337d679a226fb
tolaa <mgamtaa@gmail.com>
2023-02-16 11:22:25 +0300

    style fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,10 @@
 """hashing"""
 import bcrypt
 
-def _hash_password(password:str) -> str: 
-    """ returns bytes that is salted hash of 
+
+def _hash_password(password: str) -> str:
+    """ returns bytes that is salted hash of
         input password
     """
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    return hashed 
+    return hashed
