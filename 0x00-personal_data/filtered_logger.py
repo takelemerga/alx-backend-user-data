361307919f26bbd17a4f2ec83b62dcc8954a9ecb
tolaa <mgamtaa@gmail.com>
2023-02-22 16:40:49 +0300

    annotation fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,11 @@
 """log message"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns log message obfuscated"""
     for field in fields:
         pattern = r'({0}=)[^{1}]*({1})'.format(field, separator)
