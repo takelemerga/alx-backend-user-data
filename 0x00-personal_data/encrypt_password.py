7e49682004a53efbd38623530cf7dbe72efa02e8
tolaa <mgamtaa@gmail.com>
2023-02-23 15:58:30 +0300

    hash password added

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+!/usr/bin/env python3
+""" encripting password module """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """encript password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
