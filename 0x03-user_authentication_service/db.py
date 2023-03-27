5ead02084ce7d8dd29be3ce9d23e309288757e39
tolaa <mgamtaa@gmail.com>
2023-03-27 11:54:20 +0300

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,9 +37,13 @@ class DB:
         save the user to the database
         Return: User Object
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
         return user
 
     def find_user_by(self, **kwargs) -> User:
